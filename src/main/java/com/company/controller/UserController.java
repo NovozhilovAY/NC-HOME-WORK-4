9a9fdf16c8baa807b1d59ce4082c69243dce2d13
NovozhilovAY <sasha.n191@yandex.ru>
2021-12-14 15:56:09 +0300

    added user search

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -2,13 +2,17 @@ package com.company.controller;
 
 import com.company.model.User;
 import com.company.model.UsersStorage;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.Date;
 
 @Controller
 public class UserController {
@@ -26,5 +30,25 @@ public class UserController {
             return "user-form";
         storage.addUser(user);
         return "result";
+    }
+
+    @GetMapping("/find-user")
+    public String findUser(Model model) {
+        model.addAttribute("firstName", "");
+        model.addAttribute("lastName", "");
+        return "find-user";
+    }
+
+    @GetMapping("/find-result")
+    public String findResult(@RequestParam String firstName, @RequestParam String lastName,@RequestHeader("User-agent") String browser, Model model) {
+        User user = storage.findUser(firstName, lastName);
+        if(user != null){
+            model.addAttribute("user", user);
+            model.addAttribute("browser", browser);
+            model.addAttribute("time", new Date().toString());
+            return "find-result";
+        } else {
+            return "user-not-found";
+        }
     }
 }
